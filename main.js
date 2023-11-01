0dae08dd83907f8cc196c06d91231ee7caa93ed1
vitaodo2D <129953996+vitaodo2D@users.noreply.github.com>
2023-11-01 08:14:28 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
 function tocaSomPom(){
  document.querySelector('#som_tecla_pom') .play('')
 }
+
+const listaDeTeclas = document.querySelector(`.tecla´)
+
+
+listaDeTeclas[0].onclick = tocaSomPom;
+
+
+for(contador = 0; contador <listaDeTeclas.legth; contador++){
+  const instrumento = listaDeTeclas[contador].classList[1];
+
+   const tecla = listaDeTeclas[contador];
+
+    const idAudio = `#som_${instrumento}´;
+    tecla.onclick = function(){
+          tocaSomPom(idAudio);
+    };
+
+   tecla.onkeydown = function(){ 
+
+   }
+}
